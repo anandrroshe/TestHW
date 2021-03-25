df91ecc5cdde0fb8dbef58f784cd1c0d5c11f16f
anandrroshe <74198181+anandrroshe@users.noreply.github.com>
2021-03-25 11:30:13 +0200

    Create CacheLogger.java

---
+++ src/main/java/HW30/CacheLogger.java
@@ -0,0 +1,25 @@
+package HW30;
+
+    import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CacheLogger {
+    static Logger loggerDebug = LoggerFactory.getLogger("logger.debug");
+    static Logger loggerError = LoggerFactory.getLogger("logger.error");
+    static Logger loggerInfo = LoggerFactory.getLogger("logger.info");
+
+    public static void logDebug(String str) {
+        loggerDebug.debug(str);
+    }
+
+
+    public static void logInfo(String str) {
+        loggerInfo.info(str);
+    }
+
+
+    public static void logError(String str) {
+        loggerError.error(str);
+    }
+}
+
